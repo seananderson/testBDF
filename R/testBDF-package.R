c83bb8a89237c8030e3dd0666f4dc23ba8f684f6
Sean Anderson <sean@seananderson.ca>
2020-07-30 16:46:30 -0700

    Initial commit

---
+++ R/testBDF-package.R
@@ -0,0 +1,16 @@
+#' The 'testBDF' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name testBDF-package
+#' @aliases testBDF
+#' @useDynLib testBDF, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
+#'
+NULL
